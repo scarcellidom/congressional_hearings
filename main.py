e850717f10b50cc19ebd1aab409146babb4b3eaa
scarcellidom <137444593+scarcellidom@users.noreply.github.com>
2024-09-17 15:33:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ def loadFromURL(event):
     url = pydom["input#txt-url"][0].value
 
     log(f"Trying to fetch CSV from {url}")
-    df = pd.read_csv(open_url(url))
+    # df = pd.read_csv(open_url(url))
+    df = pd.concat([pd.read_csv(f"./assets/house_transcripts_{i}.csv",usecols=['date','youtube_id']) for i in range(1,5)])
 
     pydom["div#pandas-output"].style["display"] = "block"
     pydom["div#pandas-dev-console"].style["display"] = "block"
